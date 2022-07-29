3809dc168e6800ebc220f28e3d9f91a303189de9
hasrulwho@gmail.com <hasrulwho@gmail.com>
2022-07-29 21:24:30 +0700

    End of section 2

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return ( // yang direturn adalah jsx, meskipun mirip html, tapi ini adalah javascript
+    <div>
+      <h1>Re-store</h1>
+    </div>
+  );
+}
+
+export default App;
