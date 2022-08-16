1335603c5c0780d02744a472cee8f52205835be1
hasrulwho@gmail.com <hasrulwho@gmail.com>
2022-08-16 20:44:17 +0700

    End of section 4 React Router

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,24 +1,29 @@
 import { ThemeProvider } from "@emotion/react";
-import { createTheme } from "@mui/material";
+import { Container, createTheme } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import { useState } from "react";
+import { Route } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
 
 
 function App() { // functional component, a function that return jsx (html a look-alike)
   const [darkMode, setDarkMode] = useState(false);
-  const paletteType = darkMode ? 'dark':'light'
+  const paletteType = darkMode ? 'dark' : 'light'
   const theme = createTheme({
     palette: {
       mode: paletteType,
       background: {
-        default: paletteType === 'light' ? '#eaeaea':'#121212'
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
       }
     }
   })
 
-  function handleThemeChange(){
+  function handleThemeChange() {
     setDarkMode(!darkMode);
   }
 
@@ -26,9 +31,15 @@ function App() { // functional component, a function that return jsx (html a loo
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+        <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
         {/* <h1 style={{color:'blue'}}>Re-store</h1> */}
-        <Catalog />
+        <Container>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/catalog' component={Catalog} />
+          <Route path='/catalog/:id' component={ProductDetails} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/contact' component={ContactPage} />
+        </Container>
       </ThemeProvider>
     </>
   );
