dfe08e6bcdef99e16e1d899a7179c5e6c3d62687
hasrulwho@gmail.com <hasrulwho@gmail.com>
2022-10-29 22:38:40 +0700

    Adding an account slice & application local storage (instead of using cookies)

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -7,25 +7,26 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Paper } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import agent from '../../app/api/agent';
 import { FieldValues, useForm } from 'react-hook-form';
 import { LoadingButton } from '@mui/lab';
+import { useAppDispatch } from '../../app/store/configureStore';
+import { signInUser } from './accountSlice';
 
 const theme = createTheme();
 
 export default function Login() {
+  const history = useHistory();
+  const dispatch = useAppDispatch();
 
   const { register, handleSubmit, formState: { isSubmitting, errors, isValid } } = useForm({
-    mode: 'onTouched'
+    mode: 'all' //sebelumnya 'onTouched' artinya baru mulai memvalidasi isi form (required) setelah ngeklik dulu
   });
 
   async function submitForm(data: FieldValues) {
-    try {
-      await agent.Account.login(data);
-    } catch (error) {
-      console.log(error);
-    }
+    await dispatch(signInUser(data));
+    history.push('/catalog');
   }
 
   return (
