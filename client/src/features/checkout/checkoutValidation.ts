644fa939cec67d0507bbf4947dcec153508ffb11
hasrulwho@gmail.com <hasrulwho@gmail.com>
2022-11-13 21:31:45 +0700

    updating the payment form

--- client/src/features/checkout/checkoutValidation.ts
+++ client/src/features/checkout/checkoutValidation.ts
@@ -1,11 +1,17 @@
 import * as yup from 'yup';
 
-export const validationSchema = yup.object({
-    fullName: yup.string().required('Full name is required'),
-    address1: yup.string().required('Address line 1 is required'),
-    address2: yup.string().required(),
-    city: yup.string().required(),
-    state: yup.string().required(),
-    zip: yup.string().required(),
-    country: yup.string().required()
-})
+export const validationSchema = [
+    yup.object({ //1st step: Shipping address
+        fullName: yup.string().required('Full name is required'),
+        address1: yup.string().required('Address line 1 is required'),
+        address2: yup.string().required(),
+        city: yup.string().required(),
+        state: yup.string().required(),
+        zip: yup.string().required(),
+        country: yup.string().required()
+    }),
+    yup.object(),//2nd step: review order
+    yup.object({ //3rd step: payment details
+        nameOnCard: yup.string().required()
+    })
+]
