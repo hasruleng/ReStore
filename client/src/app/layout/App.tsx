881ff793bde7da6e6d9f4c32e329139b43b97939
hasrulwho@gmail.com <hasrulwho@gmail.com>
2022-11-20 22:29:38 +0700

    Creating the order model & orders page

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -23,6 +23,7 @@ import Login from "../../features/account/Login";
 import Register from "../../features/account/Register";
 import { fetchCurrentUser } from "../../features/account/accountSlice";
 import PrivateRoute from "./PrivateRoute";
+import Orders from "../../features/order/Orders";
 
 
 function App() { // functional component, a function that return jsx (html a look-alike)
@@ -89,6 +90,7 @@ function App() { // functional component, a function that return jsx (html a loo
             <Route path='/server-error' component={ServerError} />
             <Route path='/basket' component={BasketPage} />
             <PrivateRoute path='/checkout' component={CheckoutPage} />
+            <PrivateRoute path='/orders' component={Orders} />
             <Route path='/login' component={Login} />
             <Route path='/register' component={Register} />
             <Route component={NotFound} />
